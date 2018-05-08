de5e2e14198c0189629bb305649b11b9d00f04d9
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-05-08 20:43:33 +0900

    unresovled file resourse
    Fix

--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/loginActivity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/loginActivity.kt
@@ -3,8 +3,6 @@ package dgsw.hs.kr.gatchigachi
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import dgsw.hs.kr.gatchigachi.R
-import dgsw.hs.kr.gatchigachi.R.layout.activity_sign
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
