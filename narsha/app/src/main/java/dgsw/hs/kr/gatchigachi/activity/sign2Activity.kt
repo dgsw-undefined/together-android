1a986b85af0f10020cad14eac7c91fbef263a400
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-05-09 21:02:44 +0900

    Sign - Sign2 간 데이터 이동
    회원가입 기능

--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/sign2Activity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/sign2Activity.kt
@@ -1,13 +1,37 @@
 package dgsw.hs.kr.gatchigachi
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import dgsw.hs.kr.gatchigachi.R
+import kotlinx.android.synthetic.main.activity_sign2.*
 
 class Sign2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign2)
+        val intent = getIntent()
+        val name : String = intent.getStringExtra("name")
+        val id : String = intent.getStringExtra("id")
+        val pw : String = intent.getStringExtra("pw")
+        val pw2 : String = intent.getStringExtra("pw2")
+        val phone : String = intent.getStringExtra("phone")
+
+        btn_sign2_do_sign.setOnClickListener {
+            val nextIntent = Intent(this, LoginActivity::class.java)
+
+            val tec : String = edit_sign2_tec.text.toString()
+            val position : String = edit_sign2_position.text.toString()
+            val github : String = edit_sign2_github.text.toString()
+            val field : String = edit_sign2_field.text.toString()
+            startActivity(nextIntent)
+        }
+
+        btn_sign2_to_sign.setOnClickListener {
+            val nextIntent = Intent(this, SignActivity::class.java)
+            startActivity(nextIntent)
+        }
+
     }
 }
--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/signActivity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/signActivity.kt
@@ -3,6 +3,7 @@ package dgsw.hs.kr.gatchigachi
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import dgsw.hs.kr.gatchigachi.R
 import dgsw.hs.kr.gatchigachi.R.layout.activity_login
 import dgsw.hs.kr.gatchigachi.R.layout.activity_sign2
@@ -21,6 +22,17 @@ class SignActivity : AppCompatActivity() {
 
         btn_sign_to_sign2.setOnClickListener {
             val nextIntent = Intent(this, Sign2Activity::class.java)
+
+            val name : String = edit_sign_name.text.toString()
+            val id : String = edit_sign_id.text.toString()
+            val pw : String = edit_sign_pw.text.toString()
+            val pw2 : String = edit_sign_PWcheck.text.toString()
+            val phone : String = edit_sign_phone.text.toString()
+            nextIntent.putExtra("ToSign2",name)
+            nextIntent.putExtra("ToSign2",id)
+            nextIntent.putExtra("ToSign2",pw)
+            nextIntent.putExtra("ToSign2",pw2)
+            nextIntent.putExtra("ToSign2",phone)
             startActivity(nextIntent)
         }
     }
