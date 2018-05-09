c48724cc87f45672e08a9005d2fc4c50fd72f961
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-05-09 23:48:38 +0900

    LoginActivity Toast
    Add

--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/loginActivity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/loginActivity.kt
@@ -3,6 +3,8 @@ package dgsw.hs.kr.gatchigachi
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.Toast
 import dgsw.hs.kr.gatchigachi.R.layout.activity_sign
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -15,6 +17,25 @@ class LoginActivity : AppCompatActivity() {
         btn_login_to_sign.setOnClickListener {
             val nextIntent = Intent(this, activity_sign::class.java)
             startActivity(nextIntent)
+        }
+
+        btn_login_do_login.setOnClickListener {
+            val id : String = edit_login_id.text.toString()
+            val pw : String = edit_login_pw.text.toString()
+
+            if(id.length == 0){
+                Toast.makeText(this,"ID를 입력하세요", Toast.LENGTH_SHORT).show()
+                edit_login_id.requestFocus();
+            }
+
+            if(pw.length == 0){
+                Toast.makeText(this,"비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+                edit_login_pw.requestFocus()
+            }
+            else if(pw.length < 8){
+                Toast.makeText(this,"8자 이상의 비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+                edit_login_pw.requestFocus()
+            }
         }
     }
 }
