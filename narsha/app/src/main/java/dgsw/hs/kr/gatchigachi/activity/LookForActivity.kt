f94634beb9338d0530b1fcb0ba0e35213e2985b8
jungsunghwa <30362923+jungsunghwa@users.noreply.github.com>
2018-05-26 10:37:34 +0900

    5f

---
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/LookForActivity.kt
@@ -0,0 +1,16 @@
+package dgsw.hs.kr.gatchigachi.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dgsw.hs.kr.gatchigachi.R
+
+class LookForActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_look_for)
+
+
+
+    }
+}
