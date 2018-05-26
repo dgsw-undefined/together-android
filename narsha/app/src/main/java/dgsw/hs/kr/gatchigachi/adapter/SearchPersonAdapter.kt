593af5a74c81fd9a2416ffc184151e44abd37530
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-05-26 12:03:57 +0900

    + SearchPeron List
    Add

---
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/adapter/SearchPersonAdapter.kt
@@ -0,0 +1,43 @@
+package dgsw.hs.kr.gatchigachi.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import dgsw.hs.kr.gatchigachi.DataService.SearchUserData
+import dgsw.hs.kr.gatchigachi.R
+import dgsw.hs.kr.gatchigachi.model.User
+import dgsw.hs.kr.gatchigachi.model.UserSearch
+import kotlinx.android.synthetic.main.activity_look_for.*
+
+class SearchPersonAdapter (val context: Context, val SearchUser : ArrayList<UserSearch>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val ResultView : View = LayoutInflater.from(context).inflate(R.layout.search_result_person, null)
+
+        val UserName : TextView = ResultView.findViewById(R.id.user_name_search)
+        val Userposition : TextView = ResultView.findViewById(R.id.user_position_search)
+
+        val UserSearch = SearchUserData[position]
+
+
+        UserName.setText(UserSearch.name)
+        Userposition.setText(UserSearch.position)
+        
+
+        return ResultView
+    }
+
+    override fun getCount(): Int {
+        return SearchUserData.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return SearchUserData.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return SearchUserData.get(position).hashCode().toLong()
+    }
+}
---
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/adapter/SearchTeamAdaper.kt
@@ -0,0 +1,2 @@
+package dgsw.hs.kr.gatchigachi.adapter
+
