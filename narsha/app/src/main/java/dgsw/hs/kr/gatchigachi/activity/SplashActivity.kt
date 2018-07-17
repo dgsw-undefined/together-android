000353cb92d9dce42873842b305c1b39941b662a
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-07-18 01:46:46 +0900

    1

--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/SplashActivity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/SplashActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.AnimationUtils
 import dgsw.hs.kr.gatchigachi.LoginActivity
 import dgsw.hs.kr.gatchigachi.R
 import kotlinx.android.synthetic.main.activity_splash.*
+import android.support.v4.content.ContextCompat.startActivity
+
+
 
 class SplashActivity : AppCompatActivity() {
 
@@ -20,6 +24,13 @@ class SplashActivity : AppCompatActivity() {
         splash_video.start()
 
         val startAnim = AnimationUtils.loadAnimation(this, R.anim.splash_start)
+
+//        Handler().postDelayed({
+//            /* 메뉴액티비티를 실행하고 로딩화면을 죽인다.*/
+//            val mainIntent = Intent(this, LoginActivity::class.java)
+//            this.startActivity(mainIntent)
+//            this.finish()
+//        })
 
         splash_video.setOnClickListener {
             val nextIntent = Intent(this, LoginActivity::class.java)
