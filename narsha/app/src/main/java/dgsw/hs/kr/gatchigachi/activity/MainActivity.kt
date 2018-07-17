6987c219514d4b4f0539bdd222031160b1cb9004
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-07-18 03:05:54 +0900

    layout스덩

--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/MainActivity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/MainActivity.kt
@@ -13,6 +13,7 @@ import com.bumptech.glide.Glide
 import dgsw.hs.kr.gatchigachi.MakeTeamActivity
 import dgsw.hs.kr.gatchigachi.R
 import dgsw.hs.kr.gatchigachi.R.id.gone
+import dgsw.hs.kr.gatchigachi.R.layout.activity_main
 import dgsw.hs.kr.gatchigachi.TrustActivity
 import dgsw.hs.kr.gatchigachi.adapter.TeamGridAdapter
 import dgsw.hs.kr.gatchigachi.database.DBHelper
@@ -54,10 +55,12 @@ class MainActivity : AppCompatActivity() {
             setView(teams)
         }
 
+
         val timer = Timer()
 
         user_profile_main.background = ShapeDrawable(OvalShape())
         user_profile_main.clipToOutline = true
+        user_profile_main.bringToFront()
 
         trust_count.setOnClickListener {
             val nextIntent = Intent(this, TrustActivity::class.java)
