732d3a3bef9b359c2852e3afd737cae2fed52f3c
jungsunghwa <banusa21@dgsw.hs.kr>
2018-07-18 01:42:10 +0900

    ddd

--- narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/SplashActivity.kt
+++ narsha/app/src/main/java/dgsw/hs/kr/gatchigachi/activity/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.AnimationUtils
 import dgsw.hs.kr.gatchigachi.LoginActivity
 import dgsw.hs.kr.gatchigachi.R
@@ -11,9 +12,25 @@ import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        mDelayHandler = Handler() 
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
 
         val uri : Uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash)
         splash_video.setVideoURI(uri)
@@ -21,11 +38,11 @@ class SplashActivity : AppCompatActivity() {
 
         val startAnim = AnimationUtils.loadAnimation(this, R.anim.splash_start)
 
-        splash_video.setOnClickListener {
-            val nextIntent = Intent(this, LoginActivity::class.java)
-            startActivity(nextIntent)
-            this.finish()
-        }
+//        splash_video.setOnClickListener {
+//            val nextIntent = Intent(this, LoginActivity::class.java)
+//            startActivity(nextIntent)
+//            this.finish()
+//        }
 
 
 
